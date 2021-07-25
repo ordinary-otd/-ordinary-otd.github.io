9612c41077703882035be299ff3c17dbfc39ba3a
kyuu <58155530+jeaoq@users.noreply.github.com>
2021-07-26 00:18:15 +0700

    .

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
-function tab(t){
+window.addEventListener('hashchange', function() {
+    if(window.location.hash) {
+        tag(window.location.hash);
+    } else {
+        tag('#home');
+    }
+}, false);
+
+function tag ( t ) {
     if( $('.current').length > 0 ){
         $('.current').removeClass('current').fadeOut('fast', function() {
-            $('#' + t).fadeIn().addClass('current');
+            $(t).fadeIn().addClass('current');
         });
     } else {
-        $('#' + t).fadeIn().addClass('current');
+        $(t).fadeIn().addClass('current');
     }
-
 }
